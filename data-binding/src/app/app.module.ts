96cd5897f22c584bf1d59e25a764193fefda351a
joseademar <joseademarti@gmail.com>
2023-12-15 22:20:11 -0300

    Estudando os decorator @Input e @Output do angular

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -4,19 +4,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
+import { MeuFormModule } from './meu-form/meu-form.module';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    InputPropertyComponent,
+    OutputPropertyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MeuFormModule
   ],
   providers: [
     provideClientHydration()
   ],
+  exports:[AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,11 +10,45 @@ export class DataBindingComponent {
     url = "http:loiane.trainning.com.br";
     cursoAngular = true;
     imgCarro = "https://cdn.motor1.com/images/mgl/AkB8vL/s3/fiat-mobi-2023.jpg";
+    valorAtual = 'AB';
+    valorSalvo = '';
+    isMouseOver = false;
+    public nome: string = 'ABC';
+
+    nomeDoCurso = 'Angular';
+    valorInicial = 15;
+
+    onMudouValor(evento: any){
+      console.log(evento.novoValor);
+    }
+
+
     getValor(): number{
       return 10;
     }
 
     getCurtir(){
       return true;
+    }
+
+    botaoClicado(){
+      alert('Botão clicado!');
+    }
+
+    onKeyUp(evento: KeyboardEvent){
+      this.valorAtual = ((<HTMLInputElement>evento.target).value);
+      
+    }
+
+    salvarValor(valor: any){
+      this.valorSalvo = valor.target.value;
+    }
+
+    onMouseOverOut(){
+      this.isMouseOver = !this.isMouseOver;
+    }
+
+    getNome(nome: any){
+      this.nome = nome.target.value;
     }
 }
